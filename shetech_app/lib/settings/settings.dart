4271f81202a68cfb2824519249f116156d6d68bc
MKangabire <m.kangabire@alustudent.com>
2024-10-23 15:06:24 +0200

    settings

---
+++ shetech_app/lib/settings/settings.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'profile.dart';
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    if (index != _selectedIndex) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+
+    switch (index) {
+      case 0: // Home
+        // Add navigation to Home Screen if needed
+        break;
+      case 1: // Calendar
+        // Add navigation to Calendar Screen if needed
+        break;
+      case 2: // Setting
+        break;
+      case 3: // Profile
+        Navigator.pop(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  ShetechProfile(currentIndex: _selectedIndex)),
+        );
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 146, 100, 164),
+        title: const Text(
+          'Settings',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              // Account Section
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                margin: const EdgeInsets.only(bottom: 30),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Account',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    _buildSettingsOption(
+                      icon: Icons.person_2_outlined,
+                      label: 'Edit Profile',
+                    ),
+                    _buildSettingsOption(
+                      icon: Icons.security_update_warning_rounded,
+                      label: 'Security',
+                    ),
+                    _buildSettingsOption(
+                      icon: Icons.notifications_none,
+                      label: 'Notifications',
+                    ),
+                    _buildSettingsOption(
+                      icon: Icons.lock_outlined,
+                      label: 'Privacy',
+                    ),
+                  ],
+                ),
+              ),
+              // Support and About Section
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Support and About',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    _buildSettingsOption(
+                      icon: Icons.help_outline,
+                      label: 'Help Center',
+                    ),
+                    _buildSettingsOption(
+                      icon: Icons.info_outline,
+                      label: 'About Us',
+                    ),
+                    _buildSettingsOption(
+                      icon: Icons.contact_mail_outlined,
+                      label: 'Contact Support',
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Actions',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    _buildSettingsOption(
+                      icon: Icons.flag,
+                      label: 'Report a problem',
+                    ),
+                    _buildSettingsOption(
+                      icon: Icons.person_add_alt,
+                      label: 'Add account',
+                    ),
+                    _buildSettingsOption(
+                      icon: Icons.logout_outlined,
+                      label: 'Log out',
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 146, 100, 184),
+        selectedItemColor: const Color.fromARGB(255, 46, 45, 45),
+        unselectedItemColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today_sharp),
+            label: 'calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_3_rounded),
+            label: 'profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSettingsOption({required IconData icon, required String label}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            size: 28,
+            color: Colors.grey.withOpacity(0.6),
+          ),
+          const SizedBox(width: 15),
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
